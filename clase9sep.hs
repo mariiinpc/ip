1058b681e67d1530f80cb7bba6e8e281bd43dc88
mariiinpc <mariellalissette.vento@gmail.com>
2026-09-14 20:57:00 -0300

    Update clase9sep.hs

--- clase9sep.hs
+++ clase9sep.hs
@@ -16,6 +16,8 @@ sumatoria:: [Integer] -> Integer
 sumatoria [] = 0
 sumatoria (x:xs) = x + sumatoria xs
 
+-------------------------------------------------
+
 maximo1:: [Integer] -> Integer
 maximo1 (x:y:xs) | x > y = maximo1 (x:xs)
                  | otherwise = maximo1 (y:xs)
@@ -30,7 +32,6 @@ quitar e [] = []
 quitar e (x:xs) | e == x = xs
                 | otherwise = (x:(quitar e xs))
 
-
 ordenar:: [Integer] -> [Integer]
 ordenar [] = []
 ordenar xs = maxXs : ordenar (quitar maxXs xs)
@@ -40,3 +41,25 @@ ordenarAsc:: [Integer] -> [Integer]
 ordenarAsc [] = []
 ordenarAsc xs = ordenarAsc (quitar maxXs xs) ++ [maxXs]
     where maxXs = maximo xs
+    
+---------------------------------
+
+generarStock:: [String] -> [(String, Integer)]
+generarStock [] = []
+generarStock (x:xs) = agregarStock x (generarStock xs)
+
+agregarStock:: String -> [(String, Integer)] -> [(String, Integer)]
+agregarStock e [] = [(e,1)]
+agregarStock e ((prod, cant): xs) | e == prod = (prod, cant + 1):xs
+                                  | otherwise = (prod, cant):(agregarStock e xs)
+
+----------------
+
+cantApariciones:: Integer -> [[Integer]] -> Integer
+cantApariciones e [] = 0
+cantApariciones e (f:fs) = cantAparicionesEnFila e f + cantApariciones e fs
+
+cantAparicionesEnFila:: Integer -> [Integer] -> Integer
+cantAparicionesEnFila _ [] = 0
+cantAparicionesEnFila e (x:xs) | e == x = 1 + cantAparicionesEnFila e xs
+                               | otherwise = cantAparicionesEnFila e xs
